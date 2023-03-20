d1a10c2261fe4147e7cac442dc9bbaf36c0aa3b2
26shyamaladevi <devishyamalasankar@gmail.com>
2023-03-20 12:43:47 -0400

    Added RatingSelect & Button component, Included Real time validation, Animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import {v4 as uuidv4} from 'uuid'
 import { useState } from "react"
 import Header from "./components/Header" 
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 import FeedbackData from "./components/data/FeedbackData"
 
 
@@ -20,11 +22,20 @@ function App(){
         
     }
 
+    //Add Feedback
+    const addFeedback = (newFeedback)=>{
+        newFeedback.id = uuidv4();
+        console.log(newFeedback)
+        setFeedback([newFeedback,...feedback])
+    }
+
+
 
     return (
         <>
             <Header/>
             <div className="container">
+                <FeedbackForm handleAdd = {addFeedback}/>
                 <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback} handleDelete = {deleteFeedback}/>
             </div>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 
+import {motion, AnimatePresence} from "framer-motion"
 import PropTypes from "prop-types"
 import FeedbackItem from "./FeedbackItem"
 
@@ -6,25 +7,46 @@ function FeedbackList({feedback, handleDelete}) {
   if(!feedback || feedback.length === 0){
     return <p>No feedback to display yet!</p>
   }
-  
-  
-    return (
-    <div className='feedback-list'>
-        {feedback.map((item)=> (
-            
-            
-            <FeedbackItem key={item.id} item={item} handleDelete= {handleDelete}/>
 
+  {/* With Animation using framer-motion*/}
+  return (
+    <div className='feedback-list'>
+        <AnimatePresence>
+        {feedback.map((item)=> (
+            <motion.div 
+            key={item.id}
+            initial={{opacity:0}}
+            animate={{opacity: 1}}
+            exit={{opacity:0}}>
+                <FeedbackItem key={item.id} item={item} handleDelete= {handleDelete}/>
+
+            </motion.div> 
+            
         ) )}
+        </AnimatePresence>
+        
     </div>
   )
+  
+  
+  {/*Without Animation*/}
+//     return (
+//     <div className='feedback-list'>
+//         {feedback.map((item)=> (
+            
+            
+//             <FeedbackItem key={item.id} item={item} handleDelete= {handleDelete}/>
+
+//         ) )}
+//     </div>
+//   )
 }
 
 FeedbackList.propTypes = {
     feedback: PropTypes.arrayOf(
         PropTypes.shape(
             {
-                id: PropTypes.number.isRequired,
+                //id: PropTypes.isRequired,
                 text: PropTypes.string.isRequired,
                 rating: PropTypes.number.isRequired,
             }
