802e6541239b279acf67abadd892fcf3a52f3078
26shyamaladevi <devishyamalasankar@gmail.com>
2023-03-28 10:35:49 -0400

    reverting to older version baca86a

--- src/components/shared/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import spinner from '../assets/loading.gif'
-
-function Spinner() {
-  return (
-    <img src={spinner} alt="" style = {{width:'100px', margin:'auto',display:'block'}}/>
-  )
-}
-
-export default Spinner
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,27 +1,35 @@
 import {v4 as uuidv4} from 'uuid'
-import {createContext, useState,useEffect} from 'react'
+import {createContext, useState} from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [isLoading, setIsLoading]= useState(true);
-    const [feedback, setFeedback]= useState([])
+    const [feedback, setFeedback]= useState([
+        {
+            id:1,
+            text:'This is feedback item 1',
+            rating:10
+        },
+        {
+            id:2,
+            text:'This is feedback item 2',
+            rating:10
+        },
+        {
+            id:3,
+            text:'This is feedback item 3',
+            rating:10
+        },
+        {
+            id:4,
+            text:'This is feedback item 4',
+            rating:10
+        }
+    ])
      const[feedbackEdit, setFeedbackEit] = useState({
         item:{},
         edit: false,
      })
-
-     useEffect(()=> {
-        fetchFeedback()
-     },[])
-
-     //Fetch Feedback from Backend
-     const fetchFeedback = async() => {
-        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
-        const data = await response.json()
-        setFeedback(data)
-        setIsLoading(false)
-     }
 
      //Add Feedback
      const addFeedback = (newFeedback)=>{
@@ -66,7 +74,6 @@ export const FeedbackProvider = ({children}) => {
         editFeedback,
         feedbackEdit,//value of the editfeedback state
         updateFeedback,
-        isLoading
 
     }}>
         {children}
