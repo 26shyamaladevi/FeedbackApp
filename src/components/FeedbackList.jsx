75d7f6c7fa42f98527fbdc7217dd7d3751861e51
26shyamaladevi <devishyamalasankar@gmail.com>
2023-03-28 10:57:15 -0400

    edited db.json, feedbackList.jsx, added spinner

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,34 +3,40 @@ import {motion, AnimatePresence} from "framer-motion"
 import {useContext} from 'react'
 
 import FeedbackItem from "./FeedbackItem"
+import Spinner from "../shared/Spinner"
 import FeedbackContext from "../context/FeedbackContext"
 
 function FeedbackList() {
 
-   const {feedback} = useContext(FeedbackContext) 
-  if(!feedback || feedback.length === 0){
+   const {feedback,isLoading} = useContext(FeedbackContext) 
+  if(!isLoading && (!feedback || feedback.length === 0) ){
     return <p>No feedback to display yet!</p>
   }
 
-  /* With Animation using framer-motion*/
-  return (
-    <div className='feedback-list'>
-        <AnimatePresence>
-        {feedback.map((item)=> (
-            <motion.div 
-            key={item.id}
-            initial={{opacity:0}}
-            animate={{opacity: 1}}
-            exit={{opacity:0}}>
-                <FeedbackItem key={item.id} item={item}/>
 
-            </motion.div> 
-            
-        ) )}
-        </AnimatePresence>
-        
-    </div>
-  )
+  /* With Animation using framer-motion*/
+  
+  return isLoading ? (<Spinner/>)
+  :
+  (<div className='feedback-list'>
+  <AnimatePresence>
+  {feedback.map((item)=> (
+      <motion.div 
+      key={item.id}
+      initial={{opacity:0}}
+      animate={{opacity: 1}}
+      exit={{opacity:0}}>
+          <FeedbackItem key={item.id} item={item}/>
+
+      </motion.div> 
+      
+  ) )}
+  </AnimatePresence>
+  
+</div>)
+  
+  
+  
   
   
   /*Without Animation*/
---
+++ src/shared/Spinner.jsx
@@ -0,0 +1,9 @@
+import spinner from '../components/assests/loading.gif'
+
+function Spinner() {
+  return (
+    <img src={spinner} alt="" style = {{width:'100px', margin:'auto',display:'block'}}/>
+  )
+}
+
+export default Spinner
